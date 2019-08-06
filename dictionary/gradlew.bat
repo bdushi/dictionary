7a7b14e43bea928ab96f75ec1a92eadf2c2b9311
bruno <brddev@gmail.com>
2019-08-06 18:20:43 +0200

    working with gradle debug config

--- dictionary/gradlew.bat
+++ dictionary/gradlew.bat
@@ -30,6 +30,7 @@ set APP_BASE_NAME=%~n0
 set APP_HOME=%DIRNAME%
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+@rem set DEFAULT_JVM_OPTS=-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
 set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
 @rem Find java.exe
