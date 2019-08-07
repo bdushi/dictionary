be7c553a79e1ad9cb83e436880b713819bd8ff0a
DUSHI <U738282@ads.dlh.de>
2019-08-07 16:55:40 +0200

    Refactor

--- rxjava/src/main/java/rxjava/bruno/al/RxJava.java
+++
@@ -1,9 +0,0 @@
-package rxjava.bruno.al;
-
-public class RxJava {
-    public static void main(String args[]) {
-        System.out.println("Observable Start..");
-        ComputeFunction computeFunction = new ComputeFunction();
-        computeFunction.subjects();
-    }
-}
